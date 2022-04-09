e39832713c08bdcfce020af93eba4ba18c2300e0
MrGGLS <56687455+MrGGLS@users.noreply.github.com>
2022-04-09 14:54:50 +0800

    额，忘记之前写了啥

--- src/views/booklist/bookList.vue
+++ src/views/booklist/bookList.vue
@@ -15,63 +15,70 @@
     <el-table :data="tableData" border stripe :header-cell-style="{background:'#F2DC7C',color:'#606266'}">
 
       <el-table-column type="expand">
-        <template v-slot="tableData">
-          <el-form label-position="left" inline class="demo-table-expand">
-            <el-form-item label="id">
-              <span>{{ tableData.bookId }}</span>
+        <template v-slot="props" >
+          <el-form label-position="left" inline class="demo-table-expand" style="color: #989fa5;background: #F0F5F6" >
+            <el-form-item label="book name" style="width: 400px;height: 100px">
+              <span style="margin-top: -65px; margin-left: 50px;">{{ props.row.bookName }}</span>
             </el-form-item>
-            <el-form-item label="book name">
-              <span>{{ tableData.bookName }}</span>
+            <el-form-item label="author" style="width: 400px;height: 100px;margin-left: 200px">
+              <span style="margin-top: -65px; margin-left: 50px;">{{ props.row.bookAuthor }}</span>
             </el-form-item>
-            <el-form-item label="ISBN">
-              <span>{{ tableData.ISBN }}</span>
+            <el-form-item label="ISBN" style="width: 400px;height: 100px">
+              <span style="margin-top: -65px; margin-left: 90px;">{{ props.row.ISBN }}</span>
             </el-form-item>
-            <el-form-item label="borrow times">
-              <span>{{ tableData.borrowTimes }}</span>
+            <el-form-item label="price" style="width: 400px;height: 100px; margin-left: 200px">
+              <span style="margin-top: -65px; margin-left: 61px;">￥{{ props.row.bookPrice }}</span>
             </el-form-item>
-            <el-form-item label="book state">
-              <span>{{ tableData.bookState }}</span>
+            <el-form-item label="left num" style="width: 400px;height: 100px">
+              <span style="margin-top: -65px; margin-left: 66px;">{{ props.row.leftNum}}</span>
             </el-form-item>
-            <el-form-item label="price">
-              <span>￥{{ tableData.bookPrice }}</span>
+            <el-form-item label="cover" style="width: 200px;height: 220px; margin-left: 200px">
+              <img :src="props.row.bookImg" :alt="props.row.bookName" class="book_image" style="margin-left: 50px;">
             </el-form-item>
-            <el-form-item label="cover">
-              <img :src="tableData.bookImg" :alt="tableData.bookName">
-            </el-form-item>
-            <el-form-item label="on sale">
-              <span>{{ tableData.createTime }}</span>
+            <el-form-item style="position: relative; height: 80px;width: 300px;">
+              <el-button type="primary" v-on:click="handleBorrowBook(props.row.bookId, props.row.leftNum)">
+                <svg-icon icon-class="book" style="vertical-align: middle;">
+                  <book/>
+                </svg-icon>
+                <span>borrow</span></el-button>
+              <el-button type="success" v-on:click="handleBuyBook(props.row.bookId, props.row.leftNum)" style="margin-left: 50px; width: 124px">
+                <svg-icon icon-class="book" style="vertical-align: middle">
+                  <book/>
+                </svg-icon>
+                <span>buy</span></el-button>
             </el-form-item>
           </el-form>
         </template>
+
       </el-table-column>
 
-      <el-table-column prop="bookId" label="book id" width="150"/>
+      <!--      <el-table-column prop="bookId" label="book id" width="150"/>-->
       <el-table-column prop="bookName" label="book name" width="280"/>
       <el-table-column prop="ISBN" label="ISBN" width="280"/>
       <el-table-column prop="bookPrice" label="price" width="280"/>
-      <el-table-column prop="bookState" label="book state"/>
-      <el-table-column label="action">
-        <!--        <el-popover placement="left" :width="160">-->
-        <!--          <p>book details...</p>-->
-        <!--          <template #reference>-->
-        <!--            <el-button type="success">-->
-        <!--              <svg-icon icon-class="details" style="vertical-align: middle;">-->
-        <!--                <details/>-->
-        <!--              </svg-icon>-->
-        <!--              <span>details</span>-->
-        <!--            </el-button>-->
-        <!--          </template>-->
-        <!--        </el-popover>-->
+      <el-table-column prop="bookAuthor" label="book author"/>
+      <!--      <el-table-column label="action">-->
+      <!--        &lt;!&ndash;        <el-popover placement="left" :width="160">&ndash;&gt;-->
+      <!--        &lt;!&ndash;          <p>book details...</p>&ndash;&gt;-->
+      <!--        &lt;!&ndash;          <template #reference>&ndash;&gt;-->
+      <!--        &lt;!&ndash;            <el-button type="success">&ndash;&gt;-->
+      <!--        &lt;!&ndash;              <svg-icon icon-class="details" style="vertical-align: middle;">&ndash;&gt;-->
+      <!--        &lt;!&ndash;                <details/>&ndash;&gt;-->
+      <!--        &lt;!&ndash;              </svg-icon>&ndash;&gt;-->
+      <!--        &lt;!&ndash;              <span>details</span>&ndash;&gt;-->
+      <!--        &lt;!&ndash;            </el-button>&ndash;&gt;-->
+      <!--        &lt;!&ndash;          </template>&ndash;&gt;-->
+      <!--        &lt;!&ndash;        </el-popover>&ndash;&gt;-->
 
-        <el-button type="success" v-on:click="handleBorrowBook">
-          <svg-icon icon-class="book" style="vertical-align: middle;">
-            <book/>
-          </svg-icon>
-          <span>borrow</span></el-button>
-        <!--                      <el-button type="danger">-->
-        <!--                        <svg-icon icon-class="delete" style="vertical-align: middle;"><delete/>></svg-icon>-->
-        <!--                        <span>delete</span></el-button>-->
-      </el-table-column>
+      <!--        <el-button type="success" v-on:click="handleBorrowBook">-->
+      <!--          <svg-icon icon-class="book" style="vertical-align: middle;">-->
+      <!--            <book/>-->
+      <!--          </svg-icon>-->
+      <!--          <span>borrow</span></el-button>-->
+      <!--        &lt;!&ndash;                      <el-button type="danger">&ndash;&gt;-->
+      <!--        &lt;!&ndash;                        <svg-icon icon-class="delete" style="vertical-align: middle;"><delete/>></svg-icon>&ndash;&gt;-->
+      <!--        &lt;!&ndash;                        <span>delete</span></el-button>&ndash;&gt;-->
+      <!--      </el-table-column>-->
     </el-table>
     <div class="demo-pagination-block">
       <el-pagination
@@ -116,21 +123,24 @@ export default {
           {
             bookId: '1',
             bookName: 'vue从入门到入土',
+            bookAuthor: 'Mr.GGLS',
+            leftNum: 10,
             ISBN: '666',
-            bookState: 'available',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
-            bookImg: null,
+            bookImg: "https://img2.baidu.com/it/u=942228482,2348826349&fm=26&fmt=auto",
             createTime: '2022-02-02',
           },
           {
             bookId: '2',
             bookName: '三十年精通vue',
+            bookAuthor: 'Mr.GGLS',
+            leftNum: 0,
             ISBN: '666',
             bookState: 'available',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
-            bookImg: null,
+            bookImg: "https://64.media.tumblr.com/3a5e6d3ae5b48f51e7523c0451692de1/tumblr_p7pyh6VUfB1snerhxo4_400.jpg",
             createTime: '2022-02-02',
           },
           {
@@ -138,7 +148,7 @@ export default {
             bookName: 'vue底层源码解析',
             ISBN: '666',
             bookState: 'available',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
             bookImg: null,
             createTime: '2022-02-02',
@@ -148,7 +158,7 @@ export default {
             bookName: 'vue设计与实现',
             ISBN: '666',
             bookState: 'available',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
             bookImg: null,
             createTime: '2022-02-02',
@@ -157,7 +167,7 @@ export default {
             bookId: '512',
             bookName: '中华养生宝典',
             ISBN: '666',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
             bookImg: null,
             bookState: 'available',
@@ -165,7 +175,7 @@ export default {
           {
             bookName: '如何避免脱发',
             ISBN: '666',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
             bookImg: null,
             bookState: 'available',
@@ -173,7 +183,7 @@ export default {
           {
             bookName: '活着',
             ISBN: '666',
-            borrowTimes: 1,
+            // borrowTimes: 1,
             bookPrice: 78,
             bookImg: null,
             bookState: 'available',
@@ -214,11 +224,21 @@ export default {
       this.pageNum = pageNum
       this.load()
     },
-    handleBorrowBook(event) {
-      //get bookId
-      var bookId = event.currentTarget.parentElement.parentElement.parentElement.
-          firstElementChild.nextElementSibling.firstElementChild.innerHTML.substr(7)
-      console.log(bookId)
+    handleBorrowBook(bookId, leftNum) {
+      if(leftNum<1){
+        alert("There's no book available")
+      }
+      /*TODO
+      * check book state
+      * change book state
+      * reload page?
+      * */
+
+    },
+    handleBuyBook(bookId, leftNum) {
+      if(leftNum<1){
+        alert("There's no book on sale")
+      }
       /*TODO
       * check book state
       * change book state
@@ -245,8 +265,10 @@ export default {
 }
 
 span {
-  font-size: medium;
-  font-weight: bold;
+  font-size: large;
+  font-weight: bolder;
+  /*border-style: solid;*/
+  /*border-width: 2px;*/
 }
 
 .svg-icon {
@@ -258,6 +280,17 @@ span {
 
 .demo-pagination-block + .demo-pagination-block {
   margin-top: 10px;
+}
+
+.book_image {
+  height: 200px;
+  width: 140px;
+}
+
+.el-form-item {
+  margin-left: 50px;
+  /*border-style: solid;*/
+  /*border-width: 5px;*/
 }
 
 </style>
